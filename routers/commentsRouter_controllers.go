b651ff50bbb55c71c25df05d731965a6d3a71bdb
Mafe Avendaño <mafe.ud@gmail.com>
2018-06-27 08:28:34 -0500

    Se agrega controlador y modelo de relacion_cargos

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -127,6 +127,46 @@ func init() {
 			MethodParams: param.Make(),
 			Params: nil})
 
+	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:RelacionCargosController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
 	beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:SoporteExperienciaLaboralController"] = append(beego.GlobalControllerRouter["github.com/udistrital/experiencia_laboral_crud/controllers:SoporteExperienciaLaboralController"],
 		beego.ControllerComments{
 			Method: "Post",

b893b70e256c78cd531c1279fe4cc7492c881e38
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2021-02-22 14:21:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
   export EXPERIENCIA_LABORAL_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/campus/experiencia_laboral_crud/db/username --output text --query Parameter.Value)"
-  export EXPERIENCIA_LABORAL_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}campus//experiencia_laboral_crud/db/password --output text --query Parameter.Value)"
+  export EXPERIENCIA_LABORAL_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/campus/experiencia_laboral_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
